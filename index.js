dce5b2b48a0b3fdf7d4c2fd98135248238570036
Your Name <you@example.com>
2023-04-17 23:47:05 +0300

    difficulty logic added

---
+++ index.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const diffBox = document.querySelector('.diff__container');
+    const diffButton = document.querySelector('.diff__button');
+    let currentDifficultyBlock;
+    const removeActivity = (block) => {
+        if (!block) {
+            return;
+        }
+        block.classList.remove('diff__active');
+    }
+    const addActivity = (block) => {
+        block.classList.add('diff__active');
+        window.difficulty = block.dataset.difficulty;
+    }
+    const difficultyChoice = (event) => {
+        const target = event.target;
+        if (target === diffBox) {
+            return;
+        }
+        removeActivity(currentDifficultyBlock);
+        currentDifficultyBlock = target;
+        addActivity(currentDifficultyBlock);
+    };
+    const gameStart = () => {
+        if (!window.difficulty) {
+            return
+        }
+        window.location.pathname = './dummy.html';
+    }
+    diffBox.addEventListener('click', difficultyChoice);
+    diffButton.addEventListener('click', gameStart);
+});
